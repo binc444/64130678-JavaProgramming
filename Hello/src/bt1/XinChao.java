9cc5b414b8c798aa73617a16762603025714d38c
TrHieu <hieu.tmi.64cntt@ntu.edu.vn>
2024-09-12 10:04:37 +0700

    PUSH kho lần 1
    Tạo xong file và tên lớp

---
+++ Hello/src/bt1/XinChao.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package bt1;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
