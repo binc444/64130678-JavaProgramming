acdae820c00aeafcf788ebee74ea4e866d54cf9c
TrHieu <hieu.tmi.64cntt@ntu.edu.vn>
2024-09-12 10:08:19 +0700

    PUSH kho lần 2
    Đã xong hàm main

--- Hello/src/bt1/XinChao.java
+++ Hello/src/bt1/XinChao.java
@@ -13,7 +13,8 @@ public class XinChao {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// In ra man hinh "HelloWorld"
+		System.out.println("Hello World");
 	}
 
 }
