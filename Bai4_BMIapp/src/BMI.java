41854311c6eb89f7ae0ff85e1ecd06253e5895b6
TrHieu <hieu.tmi.64cntt@ntu.edu.vn>
2024-09-16 10:27:35 +0700

    Đã hoàn thiện bài 4

--- Bai4_BMIapp/src/BMI.java
+++ Bai4_BMIapp/src/BMI.java
@@ -15,28 +15,41 @@ public class BMI {
 		// TODO Auto-generated method stub
 		// Khai báo biến
 		float weight;
-		float heigh;
+		float height;
 		
 		// Nhận bằng bàn phím
 		Scanner banPhim = new Scanner(System.in);
 		
-		//Nhập cân nặng và chiều cao
-		System.out.print("Enter your heigh(m): ");
-		heigh = banPhim.nextFloat();
+		//Nhập chiều cao
+		do {
+			System.out.print("Enter your height(m): ");
+			height = banPhim.nextFloat();
+			if (height < 1.0 || height > 2.5) {
+                System.out.println("Invalid height! (1m -> 2.5m)");
+            }
+		} 		
+		while (height < 1.0 || height > 2.5); //nếu không đáp ứng điều kiện sẽ chạy lại từ lệnh do
 		
-		System.out.print("Enter your weight(kg): ");
-		weight = banPhim.nextFloat();
+		
+		//Nhập chiều cân nặng
+		do {
+            System.out.print("Enter your weight (kg): ");
+            weight = banPhim.nextFloat();
+            if (weight < 20 || weight > 150) {
+                System.out.println("Invalid weight! (20 -> 150kg)");
+            }
+        } while (weight < 20 || weight > 150); //nếu không đáp ứng điều kiện sẽ chạy lại từ lệnh do
 		
 		
 		//Tính BMI
-		float BMI = (weight / (heigh * heigh));
+		float BMI = (weight / (height * height));
 		
 		//Thông báo cho User chỉ số BMI
 		System.out.println("Your BMI is: " + BMI);
 		if (BMI < 18.5) {      
 	         System.out.println("You are underweight");
 	      } else if(BMI > 18.5 && BMI < 24.9) {
-		      System.out.println("Your weight is normal");
+		      System.out.println("Your BMI is normal");
 	      }
 	      else if(BMI > 25 && BMI < 29.9) {
 		      System.out.println("You are overweight");
@@ -45,7 +58,7 @@ public class BMI {
 		      System.out.println("You are obese");
 	      }
 	      else {
-		      System.out.println("You Morbidly Obese");
+		      System.out.println("You are Morbidly Obese");
 	      }
 	}
 
