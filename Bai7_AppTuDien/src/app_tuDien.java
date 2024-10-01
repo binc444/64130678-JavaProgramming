e6a93552169419722cbf880f6baacb5a2bebf557
TrHieu <hieu.tmi.64cntt@ntu.edu.vn>
2024-10-01 14:14:18 +0700

    Hoàn thành app Từ điển

--- Bai7_AppTuDien/src/app_tuDien.java
+++ Bai7_AppTuDien/src/app_tuDien.java
@@ -1,11 +1,29 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class app_tuDien {
 	public static void main(String[] args) {
 	Map<String, String> vocab = new HashMap<String, String>();
     vocab.put("Hello", "Xin chào");
     vocab.put("Apple", "Táo");
-    System.out.println(vocab.get("Hello"));
-}
+    vocab.put("Lime", "Quả chanh");
+    vocab.put("Lion", "Sư tử");
+    vocab.put("Chicken", "Con gà");
+    vocab.put("Elephant", "Voi");
+    vocab.put("Snake", "Rắn");
+    vocab.put("Tiger", "Hổ");
+    vocab.put("Pen", "Cây bút");
+    vocab.put("Programmer", "Lập trình viên");
+    
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Nhập từ cần tra: ");
+    String key = scanner.nextLine();
+
+    if (vocab.containsKey(key)) {
+        System.out.println(vocab.get(key));
+    } else {
+        System.out.println(" không có trong từ điển!!!");
+    	}	
 	}
+}
