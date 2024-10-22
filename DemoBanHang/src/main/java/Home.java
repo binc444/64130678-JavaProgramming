9df61886d3123b3b6b51cf35053668af7b35f21d
TrHieu <hieu.tmi.64cntt@ntu.edu.vn>
2024-10-22 16:39:36 +0700

    Hoàn thành demo

--- DemoBanHang/src/main/java/Home.java
+++ DemoBanHang/src/main/java/Home.java
@@ -1,9 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Home {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("hello");
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		String strConn = "jdbc:mysql://localhost:3306/demobanhang";
+		//Mở kết nối
+		Connection conn = DriverManager.getConnection(strConn, "root", "");
+		
+		// Select
+		String sqlSelect = "select * from SanPham";
+		Statement lenh = conn.createStatement();
+		ResultSet ketQua = lenh.executeQuery(sqlSelect);
+		//Hiện kết quả
+		while (ketQua.next()) {
+			int id = ketQua.getInt(1); //lấy dữ liệu cột 0
+			String tenSP = ketQua.getString(2);
+			float giaSP = ketQua.getFloat(3);
+			String mota = ketQua.getString(4);
+			System.out.print(id + "|" + tenSP + "|" + giaSP + "|" + mota + "\n");
+		}
 	}
 
 }
